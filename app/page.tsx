7c868f88b50112f198b9e2de30952660c80809dc
NataliaChiara <natalia.chiara.98@gmail.com>
2023-07-28 11:14:15 -0300

    plantilla basica

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,29 @@
 import s from "./page.module.css";
+import { dataset } from "@/lib/dataset";
 
 export default function Home() {
   return (
     <main className={s.main}>
-      <h1>hola</h1>
+      <table>
+        {dataset.horarios.map((dias) => (
+          <td key={dias.title}>
+            <th>{dias.title}</th>
+            {dias.horarios.map((horarios, index) => (
+              <tr key={index}>
+                <td> {horarios}</td>
+                <td>
+                  <tr>alumno</tr>
+                  <tr>alumno</tr>
+                  <tr>alumno</tr>
+                  <tr>alumno</tr>
+                  <tr>alumno</tr>
+                  <tr>alumno</tr>
+                </td>
+              </tr>
+            ))}
+          </td>
+        ))}
+      </table>
     </main>
   );
 }
---
+++ lib/dataset.ts
@@ -0,0 +1,28 @@
+export const dataset = {
+  horarios: [
+    {
+      title: "lunes",
+      horarios: ["17:00", "18:45", "20:30"],
+    },
+    {
+      title: "martes",
+      horarios: ["17:00", "18:45", "20:30"],
+    },
+    {
+      title: "miércoles",
+      horarios: ["17:00", "18:45", "20:30"],
+    },
+    {
+      title: "jueves",
+      horarios: ["17:00", "18:45", "20:30"],
+    },
+    {
+      title: "viernes",
+      horarios: ["17:00", "18:45", "20:30"],
+    },
+    {
+      title: "sábados",
+      horarios: ["11:15", "13:00", "14:45", "16:30"],
+    },
+  ],
+};
